6e92583a32fd196567fb2307c5d1f6fc121ed4d6
harkamalsi <harkamal_guraya@hotmail.com>
2019-05-28 22:20:42 +0200

    Updates

---
+++ precache-manifest.3cc60b0e285fe2f56a4f4126fd58a1ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8995effda93eb4756298",
+    "url": "/random-color-generator/static/js/runtime~main.74931d37.js"
+  },
+  {
+    "revision": "84291d12bdfbf8c46c59",
+    "url": "/random-color-generator/static/js/main.d1d48fa9.chunk.js"
+  },
+  {
+    "revision": "17300e9dac45011f6707",
+    "url": "/random-color-generator/static/js/2.adf063ba.chunk.js"
+  },
+  {
+    "revision": "84291d12bdfbf8c46c59",
+    "url": "/random-color-generator/static/css/main.d871117a.chunk.css"
+  },
+  {
+    "revision": "17300e9dac45011f6707",
+    "url": "/random-color-generator/static/css/2.76ae3a2c.chunk.css"
+  },
+  {
+    "revision": "7369714fd54c8fcf745b5408b8886968",
+    "url": "/random-color-generator/index.html"
+  }
+];
